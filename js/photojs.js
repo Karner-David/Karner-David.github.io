5ab48be797c7e2ad105fdaf7e53745372e65713b
Karner David <viEclipsekd@gmail.com>
2023-03-15 23:45:05 -0500

    testing javascript animation

---
+++ js/photojs.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const testElements = document.querySelectorAll('.test');
+testElements.forEach((el) => observer.observe(el));
