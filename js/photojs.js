f6e28cf54c46229c3447a9cdc3a135737e439090
Karner David <viEclipsekd@gmail.com>
2024-06-20 00:33:46 -0500

    trying to add image slider for photos

--- js/photojs.js
+++ js/photojs.js
@@ -1,14 +1,64 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
+document.addEventListener("DOMContentLoaded", function() {
+    const mainImage = document.getElementById("main_image");
+    const thumbnailSlider = document.getElementById("thumbnail_slider");
+    const prevBtn = document.getElementById("prev_btn");
+    const nextBtn = document.getElementById("next_btn");
+  
+    const photos = [
+      "../photos/asley_under_cherry_blossom_tree.jpeg",
+      "../photos/cornell_bridge_sunset.jpeg",
+      "../photos/downtown_water_reflection_1.jpeg",
+      "../photos/downtown_water_reflection_2.jpeg",
+      "../photos/downtown_water_reflection_3.jpeg",
+      "../photos/pretty_tree_coloration.jpg",
+      "../photos/seven_eleven.jpeg",
+      "../photos/street_pops.jpeg",
+      "../photos/sunrise_crabs.jpeg",
+      "../photos/sunset_fox_elem_1.jpeg",
+      "../photos/sunset_fox_elem_2.jpeg",
+      "../photos/university_federal_credit_union.jpeg"
+    ];
+  
+    let currentIndex = 0;
+  
+    function updateMainImage(index) {
+      mainImage.src = photos[index];
+      Array.from(thumbnailSlider.children).forEach((img, i) => {
+        img.classList.toggle("active", i === index);
+      });
+    }
+  
+    function populateThumbnails(startIndex) {
+      thumbnailSlider.innerHTML = "";
+      for (let i = startIndex; i < startIndex + 11; i++) {
+        const img = document.createElement("img");
+        img.src = photos[i];
+        img.addEventListener("click", () => {
+          currentIndex = i;
+          updateMainImage(currentIndex);
+        });
+        thumbnailSlider.appendChild(img);
+      }
+      updateMainImage(currentIndex);
+    }
+  
+    prevBtn.addEventListener("click", () => {
+      if (currentIndex > 0) {
+        currentIndex--;
+        if (currentIndex < photos.length - 6) {
+          populateThumbnails(currentIndex - 5);
         }
+      }
     });
-});
-
-
-const testElements = document.querySelectorAll('.test');
-testElements.forEach((el) => observer.observe(el));
+  
+    nextBtn.addEventListener("click", () => {
+      if (currentIndex < photos.length - 1) {
+        currentIndex++;
+        if (currentIndex > 5) {
+          populateThumbnails(currentIndex - 5);
+        }
+      }
+    });
+  
+    populateThumbnails(currentIndex);
+  });
