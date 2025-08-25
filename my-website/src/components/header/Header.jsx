53dbe06d54791d0d52629e4f923a8766762f8f55
Karner David <karnerdavid@Karners-MacBook-Pro.local>
2025-08-25 00:15:46 -0500

    added contacts

--- my-website/src/components/header/Header.jsx
+++ my-website/src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import headshot from '/headshot-bg.jpg';
+import GithubIcon from '../../assets/companyicons/github-icon.svg';
+import LinkedinIcon from '../../assets/companyicons/linkedin-icon.svg';
+import GmailIcon from '../../assets/companyicons/google-gmail.svg';
 import './Header.css';
 import '../../App.css';
 
@@ -10,16 +13,42 @@ export default function Header() {
         <img src={headshot} alt="Headshot" className="headshot-image" />
       </div>
       <div className="flex text-left introduction-container items-center ml-10" style={{fontFamily: 'OSBold'}}>
-        <div className="text-3xl text-white text-container">
+        <div className="text-3xl text-gray-200 text-container">
           <h1> 
-            <span className='text-4xl'>Hi, I'm <span className='text-5xl gradient-text'>Karner</span></span> <br/> 
+            <span className='text-4xl'>Hi, I'm <span className='text-5xl gradient-text'>Karner</span></span>, <br/> 
             a <span className='text-5xl gradient-text'>Software Developer</span> <br/>
             and <span className='text-4xl gradient-text'>Volleyball Lover.</span> <br/> 
-            <span className='text-4xl gradient-text'> Welcome!</span> </h1>
+            <span className='text-4xl gradient-text'> Welcome!</span> 
+          </h1>
+          <div className="flex flex-row my-4 contacts-container ">
+            <a 
+              href='https://github.com/Karner-David'
+              target="_blank"
+              rel="noopener noreferrer"
+              className='mx-4 '
+            >
+              <img src={GithubIcon} alt="github icon" className='w-9 bg-gray-200 bg-opacity-95 rounded-md p-1'/>
+            </a>
+
+            <a 
+              href='https://www.linkedin.com/in/karner-david-a37532261/'
+              target="_blank"
+              rel="noopener noreferrer"
+              className='mx-4 '
+            >
+              <img src={LinkedinIcon} alt="github icon" className='w-9'/>
+            </a>
+
+            <a 
+              href="mailto:karnerdavid7164@gmail.com"
+              className='mx-4'
+            >
+              <img src={GmailIcon} alt="github icon" className='w-9 h-9 bg-gray-200 bg-opacity-95 rounded-md p-1'/>
+            </a>
+
+          </div>
         </div>
-        <div className="contacts-container">
-          
-        </div>
+        
       </div>
     </div>
   );
