705507b8726d6cac708f5219a12358ef9ea3201f
Karner David <viEclipsekd@gmail.com>
2026-01-26 21:11:40 -0600

    added more design projects

--- my-website/src/components/DesignProjects/DesignProjects.jsx
+++ my-website/src/components/DesignProjects/DesignProjects.jsx
@@ -6,6 +6,8 @@ import Spider from '/Graphics/krita_spider-final.jpg';
 import Rainy from '/Graphics/RainyDay-final.png';
 import Samurai from '/Graphics/sam-final.png';
 import Zombie from '/Graphics/ZombieBrutalism.png';
+import Red from '/Graphics/Red-Final-true.png';
+import Car from '/Graphics/carSample.png';
 import Image from './Image';
 
 import '../../App.css';
@@ -17,17 +19,19 @@ export default function DesignProjects() {
             <h1 className='flex text-5xl text-center my-10'> Design Projects </h1>
             <div className='grid grid-cols-3 mx-20 mb-20 gap-5'>
                 <div className='flex flex-col gap-5'>
+                    <Image src={Red} alt=""></Image>
                     <Image src={BSRPoster} alt="" />
                     <Image src={Breathe} alt="" />
                 </div>
                 <div className='flex flex-col gap-5'>
-                    <Image src={Cyber} alt="" />
-                    <Image src={Spider} alt="" />
+                    <Image src={Car} alt="" />
                     <Image src={Rainy} alt="" />
+                    <Image src={Spider} alt="" />
                 </div>
                 <div className='flex flex-col gap-5'>
                     <Image src={Samurai} alt="" />
                     <Image src={Zombie} alt="" />
+                    <Image src={Cyber} alt="" />
                 </div>
             </div>
 
