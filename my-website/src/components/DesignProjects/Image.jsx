6b3641435ca8f37261ed4fd869eb552ed85a1ba5
Karner David <viEclipsekd@gmail.com>
2026-01-30 14:12:31 -0600

    added ability to click on image to focus on it

--- my-website/src/components/DesignProjects/Image.jsx
+++ my-website/src/components/DesignProjects/Image.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+export default function Image({ src }) {
+    const [isOpen, setIsOpen] = useState(false);
 
-export default function Image({src}) {
     return (
-        <div className="w-[300px] h-[400px]" >
-            <img src={src} alt="" 
-            className="w-full h-full object-contain"
-            />
-        </div>
-    )
+        <>
+            <div 
+                className="w-[300px] h-[400px] cursor-pointer hover:scale-105 duration-300" 
+                onClick={() => setIsOpen(true)}
+            >
+                <img 
+                    src={src} 
+                    alt="" 
+                    className="w-full h-full object-contain"
+                />
+            </div>
+
+            {isOpen && (
+                <div 
+                    className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm"
+                    onClick={() => setIsOpen(false)}
+                >
+                    <img 
+                        src={src} 
+                        alt="" 
+                        className="max-w-[90%] max-h-[90%] object-contain shadow-2xl"
+                        onClick={(e) => e.stopPropagation()} 
+                    />
+                </div>
+            )}
+        </>
+    );
 }
